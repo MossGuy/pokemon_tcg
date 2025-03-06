88d42231cfc9e49014bdd7b1af57059980cf47c2
visiegroep-noname <milanpallas@hotmail.nl>
2025-03-06 12:23:05 +0100

    api aanroepen
    code om de api aan te roepen toe gevoegd. id meegegeven met link in kaarten.php

--- kaart.php
+++ kaart.php
@@ -1,3 +1,16 @@
+<?php
+include "./api_key.php";
+if (isset($_GET['id'])) {
+    define("CARD_ID", $_GET['id']);
+}
+
+// define("URL", "https://api.pokemontcg.io/v2/cards?q=id:" . CARD_ID . KEY);
+define("URL", "./test_json_bestanden/pokemon_card.json");
+$response = file_get_contents(URL);
+$data = json_decode($response, true);
+
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
