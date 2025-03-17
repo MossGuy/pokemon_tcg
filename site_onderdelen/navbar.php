86bba70fcc52c1483b3e30c23a8d65e17712d1e7
nouser <keanu2503@gmail.com>
2025-03-17 10:30:01 +0100

    icons folder toegevoegd. navbar gevuld met links en input veld

--- site_onderdelen/navbar.php
+++ site_onderdelen/navbar.php
@@ -1,8 +1,14 @@
 <nav>
-    <div class="container">
-        <p></p>
-        <div class="links">
-            <p><a href="./sets.php">Sets</a></p>
+    <div class="container flex_row">
+        <a class="flex_row i_center" href="./index.php">
+            <img class="nav_icon" src="./images/icons/icon_1.png" alt="">
+            Pokemon TCG bibliotheek
+        </a>
+        <form action="" method="POST">
+            <input id="card_query" name="card_query" type="text" placeholder="Zoek een kaart">
+        </form>
+        <div class="links flex_row i_center">
+            <a href="./sets.php">Sets</a>
         </div>
     </div>
 </nav>
