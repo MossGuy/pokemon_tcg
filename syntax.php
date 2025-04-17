c1e1b28773844344ff3b28e04c72565c00bd0ea0
visie_backend <>
2025-04-17 13:12:26 +0200

    zoek_formulier.php aangemaakt een styletweak gemaakt in syntax.php

--- syntax.php
+++ syntax.php
@@ -20,42 +20,36 @@
 
     <main>
         <section class="container">
-            <div class="textblock">
-                <h1>Syntax handleiding</h1>
-                <p>
-                    De pokemon kaarten bibliotheek heeft verschillende manieren om data op te vragen.
-                    Om complexe queries (zoek opdrachten) te maken, heb je twee opties:
-                </p>
-            </div>
+            <h1>Syntax handleiding</h1>
+            <p>
+                De pokemon kaarten bibliotheek heeft verschillende manieren om data op te vragen.
+                Om complexe queries (zoek opdrachten) te maken, heb je twee opties:
+            </p>
         </section>
 
         <section class="container">
-            <div class="textblock">
-                <h1>Optie 1: Het geavanceerde zoek formulier</h1>
-                <p>
-                    Een makkelijke manier om te zoeken naar kaarten is het <a class="accent_hover" href=""><strong>Geavanceerde zoek formulier</strong></a>.
-                    Dit maakt het mogelijk om snel en makkelijk kaarten te zoeken gebaseerd op verschillende criteria zoals naam, set, zwakte en meer.
-                </p>
-            </div>
+            <h1>Optie 1: Het geavanceerde zoek formulier</h1>
+            <p>
+                Een makkelijke manier om te zoeken naar kaarten is het <a class="accent_hover" href=""><strong>Geavanceerde zoek formulier</strong></a>.
+                Dit maakt het mogelijk om snel en makkelijk kaarten te zoeken gebaseerd op verschillende criteria zoals naam, set, zwakte en meer.
+            </p>
         </section>
 
         <section class="container">
-            <div class="textblock">
-                <h1>Optie 2: De geavanceerde query syntax</h1>
-                <p>
-                    De Pokémon kaarten bibliotheek maakt gebruik van een query syntax mogelijk gemaakt door de <a class="accent_hover" href="https://pokemontcg.io/"><strong>Pokémon TCG API</strong></a>.
-                    Deze syntax is gebaseerd op <a class="accent_hover" href="https://www.lucenetutorial.com/lucene-query-syntax.html"><strong>Lucene</strong></a> dat complexe queries kan uitvoeren en ondersteund operators als AND, OR, NOT en meer.
-                </p>
-                <br>
-                <p>
-                    Je kan deze complexe queries overal gebruiken op de Pokémon kaarten bibliotheek. Bijvoorbeeld de query <span class="accent"><strong>subtypes:tera</strong></span> zoekt alle kaarten die als subtype 'Tera' hebben.
-                    De zoekbalken hebben als standaard output <span class="accent"><strong>name:"jouw query"</strong></span> tenzei er leestekens worden gebruikt.
-                </p>
-                <br>
-                <p>
-                    Bekijk de <a class="accent_hover" href="https://docs.pokemontcg.io/"><strong>API documentatie</strong></a> voor meer details en voorbeelden.
-                </p>
-            </div>
+            <h1>Optie 2: De geavanceerde query syntax</h1>
+            <p>
+                De Pokémon kaarten bibliotheek maakt gebruik van een query syntax mogelijk gemaakt door de <a class="accent_hover" href="https://pokemontcg.io/"><strong>Pokémon TCG API</strong></a>.
+                Deze syntax is gebaseerd op <a class="accent_hover" href="https://www.lucenetutorial.com/lucene-query-syntax.html"><strong>Lucene</strong></a> dat complexe queries kan uitvoeren en ondersteund operators als AND, OR, NOT en meer.
+            </p>
+            <br>
+            <p>
+                Je kan deze complexe queries overal gebruiken op de Pokémon kaarten bibliotheek. Bijvoorbeeld de query <span class="accent"><strong>subtypes:tera</strong></span> zoekt alle kaarten die als subtype 'Tera' hebben.
+                De zoekbalken hebben als standaard output <span class="accent"><strong>name:"jouw query"</strong></span> tenzei er leestekens worden gebruikt.
+            </p>
+            <br>
+            <p>
+                Bekijk de <a class="accent_hover" href="https://docs.pokemontcg.io/"><strong>API documentatie</strong></a> voor meer details en voorbeelden.
+            </p>
         </section>
     </main>
 
