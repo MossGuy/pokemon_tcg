b83283c2e86d05f88a99302761a437b596fd8b44
visie_backend <>
2025-04-17 16:20:10 +0200

    navbar collapsable gemaakt

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 // console.log("globale javascript bestand is goed gekoppeld!");
 
-// Globale functie
-function toggle_menu(id, style, arrow_id) {
-    const e = document.getElementById(id);
-    const displayValue = window.getComputedStyle(e).display;
+// De navbar toggle
+const toggleBtn = document.getElementById('navToggle');
+const navContent = document.getElementById('navContent');
 
-    e.style.display = (displayValue === "none") ? style : "none";
-
-    if (arrow_id !== undefined) {toggle_arrow_icon(arrow_id)}
-}
-
-function toggle_arrow_icon(id) {
-    const e = document.getElementById(id);
-    e.classList.toggle("bi-arrow-down-short");
-    e.classList.toggle("bi-arrow-up-short");
-}
+toggleBtn.addEventListener('click', () => {
+    navContent.classList.toggle('active');
+});
